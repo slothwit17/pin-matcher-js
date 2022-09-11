5365b935ba0cf98bffbe14cb8a6cc79b1b3ee0fd
slothwit <rakibuzzaman16-408@diu.edu.bd>
2022-09-12 02:07:35 +0600

    attempt is not working

--- custom.js
+++ custom.js
@@ -1,25 +1,71 @@
+//pin generate btn event handler
 document.getElementById('pin-generate-btn').addEventListener('click',function(){
     var randomNumber = Math.random() * 9000;
     var output = 1000 + Math.round(randomNumber);
     document.getElementById('ganerated-pin').value = output;
 })
 
+
+//clear and back space btn event handler
+document.getElementById('cal-main').addEventListener('click', function(event){
+    const num = event.target.innerText;
+    const typedNumber = document.getElementById('numDisplay');
+    const preTypedNumber = typedNumber.value;
+
+    if(num == '<'){
+        const digit = preTypedNumber.split('');
+        digit.pop();
+        const remainingDigit = digit.join('');
+        typedNumber.value = remainingDigit;
+    }
+
+    else if(num == "C"){
+        typedNumber.value = '';
+    }
+
+    else if(num == "Submit"){
+        
+    }
+    
+    
+})
+
+// display numbers 
 function display(value){
     const numbers = document.getElementById('numDisplay');
     numbers.value = numbers.value + value;
 }
 
-const generateNumber = document.getElementById('ganerated-pin').value;
-const inputNumber = document.getElementById('numDisplay').value;
 
-document.getElementById('submit').addEventListener('click', function(){
-    if(generateNumber == inputNumber){
-        var rightNotify = document.getElementById('rightPinNotify');
-        rightNotify.style.display = "block";
-    }
+//submit btn event handler
+document.getElementById('cal-main').addEventListener('click', function(event){
     
-    else{
-        var wrongNotify = document.getElementById('wrongPinNotify');
-        wrongNotify.style.display = "block";
+    var attempt = 3;
+    const btn = event.target.innerText;
+    const generateNumber = document.getElementById('ganerated-pin').value;
+    const inputNumber = document.getElementById('numDisplay').value;
+    const rightNotify = document.getElementById('rightPinNotify');
+    const wrongNotify = document.getElementById('wrongPinNotify');
+    const attemptLeft = document.getElementById('attemtCount');
+
+    if (btn == "Submit"){
+        if(generateNumber == inputNumber){   
+            rightNotify.style.display = "block";  
+            wrongNotify.style.display = "none";
+            
+        }
+        
+        else{
+            rightNotify.style.display = "none";
+            wrongNotify.style.display = "block";
+            attempt --;          
+            console.log(attempt); 
+            if (attempt == 0) {
+                document.getElementById('submit').disabled = true;
+                return false;
+            }
+        }
     }
+
+    
 })
